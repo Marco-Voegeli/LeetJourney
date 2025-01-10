c069a82dddc9b963cdb4f913337cddc9998ca28d
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2025-01-10 16:38:23 +0100

    Time: 239 ms (64.72%), Space: 32.2 MB (34.05%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        buyPrice = prices[0]
-        buyPrice_idx = 0
         memoizProfits = [[0 for i in range(len(prices))] for i in range(2)] # First price can't make more money
         # first pass
         for i,price in enumerate(prices):
+            if i == 0 or i == 1:
+                buyPrice = prices[0]
             if price < buyPrice:
-                buyPrice_idx = i
                 buyPrice = price
             memoizProfits[0][i] = max(price - buyPrice, memoizProfits[0][i-1]) 
         # 2nd pass
