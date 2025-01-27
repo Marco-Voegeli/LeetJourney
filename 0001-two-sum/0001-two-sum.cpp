db20c2e23c07721275c4f8361dfa9b6e2049c942
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2025-01-27 10:45:56 +0100

    Time: 3 ms (74.73%), Space: 15 MB (21.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,16 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         std::map<int, int> valIndexMap;
         for (int i = 0; i < nums.size(); i++){
-                valIndexMap[nums[i]] = i;
-        }
-        for (int i = 0; i < nums.size(); i++){
-            if (valIndexMap.find(target - nums[i]) != valIndexMap.end()) {
-                int mapIdx = valIndexMap.at(target-nums[i]);
-                if (mapIdx != i){
-                    return {i, mapIdx};
-                }
+            int complement = target - nums[i];
+            if (valIndexMap.find(complement) != valIndexMap.end()){
+                return {valIndexMap[complement], i};
             }
-        } 
-    return nums;
+            valIndexMap[nums[i]] = i;
+
+        }
+        return {};
     }
 };
