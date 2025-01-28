984d491f08563d0fcb6afd9d1c674a8c869afead
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2025-01-28 21:34:14 +0100

    Time: 0 ms (100%), Space: 97.6 MB (7.27%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int maxProfit = 0;
         for (int i = 1; i < prices.size(); i++){
             if (prices[i] < minPrice){
-                minPrice = price;
+                minPrice = prices[i];
             }
             else if(prices[i] - minPrice > maxProfit){
                 maxProfit = prices[i] - minPrice;
