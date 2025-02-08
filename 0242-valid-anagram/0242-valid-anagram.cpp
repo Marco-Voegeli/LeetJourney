ba264c0eaf4196aafa2fba640ce6f3eba986e709
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2025-02-08 09:13:01 +0100

    Time: 0 ms (100%), Space: 9.9 MB (11.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,15 @@
-#include <map>
+#include <unordered_map>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.length() != t.length()){
             return false;
         }
-        std::map<char, int> wordCount2;
-        std::map<char, int> wordCount1;
+        unordered_map<char, int> wordCount2;
+        unordered_map<char, int> wordCount1;
         for(int i = 0; i < s.length(); i++){
             wordCount2[s[i]] += 1;
             wordCount1[t[i]] += 1;
-
         }
         return wordCount2 == wordCount1;
     }
