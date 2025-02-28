4d101d926611c9949857dc2d7cfff9d55d31a073
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2025-02-28 10:40:48 +0100

    Time: 0 ms (100%), Space: 17.7 MB (79.43%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -6,13 +6,8 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # Move the first pointer at half the speed of the second one
-        p1 = head
-        p2 = head
-        while p2.next:
-            p2 = p2.next
-            p1 = p1.next
-            if p2.next:
-                p2 = p2.next
-            
-        return p1
-            
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
