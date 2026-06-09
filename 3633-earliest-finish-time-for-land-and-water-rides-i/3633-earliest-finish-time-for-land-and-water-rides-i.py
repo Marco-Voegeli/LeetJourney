92a87d61e60c75bd3c4aba2e83b88952efc53863
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-06-09 14:12:54 +0800

    Time: 3 ms (97.72%), Space: 19.3 MB (85.1%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def earliestFinishTime(self, landStartTime: List[int], landDuration: List[int], waterStartTime: List[int], waterDuration: List[int]) -> int:
+        '''
         endTimes = []
         for st, d in zip(landStartTime, landDuration):
             landEndTime = st + d
@@ -12,3 +13,13 @@ class Solution:
                 finalEndTime = max(waterEndTime, lSt) + lD        
                 endTimes.append(finalEndTime)
         return min(endTimes)
+        '''
+        landEndTime = min(st+d for st,d in zip(landStartTime, landDuration))
+        waterEndTime = min(st+d for st,d in zip(waterStartTime, waterDuration))
+
+        ans = 9999999
+        for st,d in zip(waterStartTime, waterDuration):
+            ans = min(ans, max(landEndTime, st) + d)
+        for st,d in zip(landStartTime, landDuration):
+            ans = min(ans, max(waterEndTime, st) + d)
+        return ans
