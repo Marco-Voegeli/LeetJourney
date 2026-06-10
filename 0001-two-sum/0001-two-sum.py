e662d08339c3924dbd6d00cec097efe1446d26b2
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-06-10 12:51:58 +0800

    Time: 4 ms (39.71%), Space: 20.7 MB (8.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,13 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        number_map = {}
-        for i, num in enumerate(nums):
-            new_target = target-num
-
-            if new_target in number_map:
-                return [i, number_map[new_target]]
-            number_map[num]= i
-        return None
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted_nums = sorted(enumerate(nums), key=lambda item: item[1])
+        lhs_i = 0
+        rhs_i = len(sorted_nums) - 1
+        curr_val = sorted_nums[lhs_i][1] + sorted_nums[rhs_i][1]
+        while curr_val != target:
+            if curr_val < target:
+                lhs_i = lhs_i + 1
+            else:
+                rhs_i = rhs_i - 1
+            curr_val = sorted_nums[lhs_i][1] + sorted_nums[rhs_i][1]
+        return [sorted_nums[lhs_i][0], sorted_nums[rhs_i][0]]
