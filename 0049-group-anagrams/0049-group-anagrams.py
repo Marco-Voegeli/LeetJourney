d8cc771d66b22ac5141e6c8cd85e6a07f07bc864
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-06-18 12:20:38 +0800

    Time: 13 ms (50.99%), Space: 22.4 MB (43.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,35 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def heatmap(x):
-            heatmap = {}
-            for c in x:
-                if c in heatmap:
-                    heatmap[c] += 1
-                else:
-                    heatmap[c] = 1
-            return heatmap
-        def compare_heatmap(h1, h2):
-            if not h1 and h2:
-                return False
-            if len(h1) != len(h2):
-                return False
-            for (key, val) in h1.items():
-                if key not in h2:
-                    return False
-                if h2[key] != val:
-                    return False
-            return True
-        strs_with_heatmap = [(heatmap(x), x) for x in strs]
-        result = []
-        for (heatmap, string) in strs_with_heatmap:
-            added = False
-            if not result:
-                result.append((heatmap, [string]))
-                continue
-            for (heatmap_base, str_ls) in result:
-                if compare_heatmap(heatmap_base, heatmap):
-                    str_ls.append(string)
-                    added = True
-            if not added:
-                result.append((heatmap, [string]))
-        return [x[1] for x in result]
+       # Sorted strings are equal if anagrams
+        result = {}
+        for string in strs:
+            sort_str = tuple(sorted(string))
+            if sort_str not in result:
+                result[sort_str] = [string]
+            else: 
+                result[sort_str].append(string)
+        res = [v for k, v in result.items()]
+        return res
