4cd4d39e9011eb54683cc698509715da14cc82cb
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-06-26 16:25:12 +0800

    Time: 293 ms (14.09%), Space: 19.6 MB (93.84%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # Move right pointer until all of t
+        # Move left hand point until u hit a elem of t - record length
+        # Move right pointer again until all of t
+        t_counted = {}
+        for c in t:
+            if c not in t_counted:
+                t_counted[c] = 1
+            else:
+                t_counted[c] += 1
+
+        lhs = 0
+        rhs = 0
+        min_string = s + "a"
+
+        while rhs != len(s):
+            while rhs < len(s) and max(t_counted.values()) > 0:
+                if s[rhs] in t_counted:
+                    t_counted[s[rhs]] -= 1
+                rhs += 1 
+            print("RHS: ", rhs)
+            while lhs < rhs and max(t_counted.values()) <= 0:
+                if s[lhs] in t_counted:
+                    t_counted[s[lhs]] += 1
+                    new_substring = s[lhs:rhs] 
+                    min_string = new_substring if len(new_substring) < len(min_string) else min_string
+                lhs += 1
+            print("LHS: ", lhs)
+        return min_string if len(min_string) <= len(s) else ""
+                
