dd98c93e152a943717ef10a5520624fb7dbb9eba
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-07-13 14:48:37 +0800

    Time: 75 ms (10.31%), Space: 25.5 MB (58.92%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,26 +1,43 @@
+import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
+        self.nums = sorted(nums)[-k:]
         self.k = k
-        nums_ordered = sorted(nums, reverse=True)
-        self.nums = nums_ordered[:k]
+
+    def push(self, val:int) -> int:
+        self.nums.append(val)
+        i = len(self.nums) - 1
+        while i > 0:
+            parent_i = (i - 1) // 2 
+            if self.nums[i] < self.nums[parent_i]:
+                self.nums[i],self.nums[parent_i] = self.nums[parent_i], self.nums[i]
+            i = parent_i
+        return self.nums[0]
+    
+    def pop(self) -> int:
+        last = self.nums.pop()
+        self.nums[0] = last
+        i = 0
+        while True:
+            c_1, c_2 = 2*i+1, 2*i + 2
+            child = i
+            len_nums = len(self.nums)
+            if c_1 < len_nums and self.nums[c_1] < self.nums[child]:
+                child = c_1
+            if c_2 < len_nums and self.nums[c_2] < self.nums[child]:
+                child = c_2
+            if child == i:
+                break
+            self.nums[i], self.nums[child] = self.nums[child], self.nums[i]
+            i = child
+        return self.nums[0]
 
     def add(self, val: int) -> int:
-        if not self.nums:
-            self.nums.append(val)
-            return self.nums[-1]
-        if val <= self.nums[-1]:
-            if len(self.nums) < self.k:
-                self.nums.append(val)
-                return val
-            return self.nums[-1]
-        for i, num in enumerate(self.nums):
-            if val > num:
-                self.nums[i+1:] = self.nums[i:max(self.k -1, len(self.nums)-1)] # Drop last elem
-                self.nums[i] = val
-                return self.nums[-1]
-
-
+        self.push(val)
+        if len(self.nums) > self.k:
+            return self.pop()
+        return self.nums[0]
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
