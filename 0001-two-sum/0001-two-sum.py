2a1234f227c266bd90ad35fc273dc13701482703
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-09-21 13:52:27 +0200

    Time: 6 ms (36.36%), Space: 20.7 MB (7.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_nums = sorted(enumerate(nums), key=lambda item: item[1])
-        lhs_i = 0
-        rhs_i = len(sorted_nums) - 1
-        curr_val = sorted_nums[lhs_i][1] + sorted_nums[rhs_i][1]
-        while curr_val != target:
-            if curr_val < target:
-                lhs_i = lhs_i + 1
-            else:
-                rhs_i = rhs_i - 1
-            curr_val = sorted_nums[lhs_i][1] + sorted_nums[rhs_i][1]
-        return [sorted_nums[lhs_i][0], sorted_nums[rhs_i][0]]
+        p1 = 0
+        p2 = len(nums) - 1 
+        nums_idx = [(num ,i) for (i, num) in enumerate(nums)]
+        list.sort(nums_idx)
+        while p1 < p2:
+            num_1, idx_1 = nums_idx[p1]
+            num_2, idx_2 = nums_idx[p2]
+            output = num_1 + num_2
+            if output == target:
+                return [idx_1, idx_2]
+            elif output > target:
+                p2 -= 1
+            elif output < target:
+                p1 += 1
+        raise "no solution found"
+            
+
