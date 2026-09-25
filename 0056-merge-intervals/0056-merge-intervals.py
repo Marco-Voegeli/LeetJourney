10d01daf886c972f5a13b804ed1a1c6077827f74
Marco Voegeli <31365321+Marco-Voegeli@users.noreply.github.com>
2026-09-25 18:46:28 +0200

    Time: 0 ms (100%), Space: 22.3 MB (87.37%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -5,12 +5,12 @@ class Solution:
         for intv in intervals:
             if not res:
                 res.append(intv)
-            res_elem = res[-1] # Has the smallest start interval
-            if res_elem[1] >= intv[0]:
-                res[-1] = [res_elem[0], max(res_elem[1], intv[1])]
-            if res_elem[1] < intv[0]: 
+            res_intv = res.pop()
+            if res_intv[1] >= intv[0]:
+                res.append([res_intv[0], max(intv[1], res_intv[1])])
+            else:
+                res.append(res_intv)
                 res.append(intv)
         return res
-
 
             
